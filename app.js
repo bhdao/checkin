0ecf31388ea01a79fa88ffe7e5990349739989c1
brendan <onthedaolow@gmail.com>
2021-11-04 21:59:21 -0700

    Init commit. ver 1.0

---
+++ app.js
@@ -0,0 +1,74 @@
+
+let data;
+if (JSON.parse(localStorage.getItem("checkinData"))) {
+  data = JSON.parse(localStorage.getItem("checkinData"))
+} else { data = {} };
+
+const DateString = (date) => {
+  return `${date[0]} ${date[1]}, ${date[2]}`
+};
+
+const DateStringProp = (date) => {
+
+  return `${date[0]}${date[1]}${date[2]}`
+}
+
+const GetDate = (date, format) => {
+  let day = today.getDate();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  if (!format) {
+    return [Months[month], day, year]
+  }
+};
+
+const UpdateStorage = () => {
+
+  data[DateController.DateStringProp(date)] = DateController.GetDate();
+  localStorage.setItem("checkinData", JSON.stringify(data))
+}
+
+const checkinContainer = document.getElementById("calendarBody");
+
+const CreateCheckinButton = () => {
+  const buttonBody = document.createElement("div");
+  buttonBody.textContent = "Click to Check In";
+  buttonBody.classList.add("checkinButton");
+  buttonBody.addEventListener("click", (x) => {
+    Checkin(date);
+    x.target.remove();
+  });
+  document.querySelector("body").appendChild(buttonBody);
+}
+
+const Checkin = (date) => {
+  const checkinBadge = document.createElement("div");
+  checkinBadge.classList.add(`checkinBadge`);
+  checkinBadge.textContent = DateString(date);
+  checkinContainer.appendChild(checkinBadge);
+  UpdateStorage();
+}
+
+const DateController = { "DateString": DateString, "DateStringProp": DateStringProp, "GetDate": GetDate };
+
+const Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+let today = new Date;
+
+let date = GetDate(today);
+
+const initPage = () => {
+
+};
+
+for (const x in data) {
+  Checkin(data[x]);
+}
+
+if (!data.hasOwnProperty(DateStringProp(date))) {
+  CreateCheckinButton();
+}
+
+const change = () => {
+  today.setDate(12);
+  date = GetDate(today);
+};
